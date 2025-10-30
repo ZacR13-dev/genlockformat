0fec8c05685c827940227e4884bd18957043800b
ZacR13-dev <reis.kevin13@gmail.com>
2025-10-30 19:36:50 +0100

    fix: Correct max Pokédex for HeartGold to 251

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@ const availableGames = [
     { name: 'Pokémon Rouge Feu', image: 'image-jeu/224.png', gen: 3, maxDex: 151, spriteFolder: 'Gen3/firered-leafgreen' },
     { name: 'Pokémon Émeraude', image: 'image-jeu/pokemon_emeraude.png', gen: 3, maxDex: 386, spriteFolder: 'Gen3/emerald' },
     { name: 'Pokémon Platine', image: 'image-jeu/pokemon_platine.jpg', gen: 4, maxDex: 493, spriteFolder: 'Gen4/diamond-pearl' },
-    { name: 'Pokémon HeartGold', image: 'image-jeu/81ESDoG1PVL.webp', gen: 4, maxDex: 256, spriteFolder: 'Gen4/heartgold-soulsilver' },
+    { name: 'Pokémon HeartGold', image: 'image-jeu/81ESDoG1PVL.webp', gen: 4, maxDex: 251, spriteFolder: 'Gen4/heartgold-soulsilver' },
     { name: 'Pokémon SoulSilver', image: '', gen: 4, maxDex: 493, spriteFolder: 'Gen4/heartgold-soulsilver' },
     { name: 'Pokémon Noir', image: 'image-jeu/pokemon_version_noire.jpg', gen: 5, maxDex: 649, spriteFolder: 'Gen5' },
     { name: 'Pokémon Blanc', image: '', gen: 5, maxDex: 649, spriteFolder: 'Gen5' },
