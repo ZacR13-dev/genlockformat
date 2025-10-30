f5c4a5a590988470c652a61bd95acd7fb603b180
ZacR13-dev <reis.kevin13@gmail.com>
2025-10-30 19:34:49 +0100

    fix: Correct max Pokédex number for FireRed and HeartGold

--- js/config.js
+++ js/config.js
@@ -6,10 +6,10 @@
 const availableGames = [
     { name: 'Pokémon Jaune', image: 'image-jeu/pokemon_jaune.png', gen: 1, maxDex: 151, spriteFolder: 'Gen1/yellow' },
     { name: 'Pokémon Cristal', image: 'image-jeu/pokemon_cristal.png', gen: 2, maxDex: 251, spriteFolder: 'Gen2/crystal' },
-    { name: 'Pokémon Rouge Feu', image: 'image-jeu/224.png', gen: 3, maxDex: 386, spriteFolder: 'Gen3/firered-leafgreen' },
+    { name: 'Pokémon Rouge Feu', image: 'image-jeu/224.png', gen: 3, maxDex: 151, spriteFolder: 'Gen3/firered-leafgreen' },
     { name: 'Pokémon Émeraude', image: 'image-jeu/pokemon_emeraude.png', gen: 3, maxDex: 386, spriteFolder: 'Gen3/emerald' },
     { name: 'Pokémon Platine', image: 'image-jeu/pokemon_platine.jpg', gen: 4, maxDex: 493, spriteFolder: 'Gen4/diamond-pearl' },
-    { name: 'Pokémon HeartGold', image: 'image-jeu/81ESDoG1PVL.webp', gen: 4, maxDex: 493, spriteFolder: 'Gen4/heartgold-soulsilver' },
+    { name: 'Pokémon HeartGold', image: 'image-jeu/81ESDoG1PVL.webp', gen: 4, maxDex: 256, spriteFolder: 'Gen4/heartgold-soulsilver' },
     { name: 'Pokémon SoulSilver', image: '', gen: 4, maxDex: 493, spriteFolder: 'Gen4/heartgold-soulsilver' },
     { name: 'Pokémon Noir', image: 'image-jeu/pokemon_version_noire.jpg', gen: 5, maxDex: 649, spriteFolder: 'Gen5' },
     { name: 'Pokémon Blanc', image: '', gen: 5, maxDex: 649, spriteFolder: 'Gen5' },
